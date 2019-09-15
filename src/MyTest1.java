2b1b2aaf63523b7744eda5cbfc1aa3e06a1735a8
lufeng <you@example.com>
2019-09-15 16:33:19 +0800

    init

---
+++ src/MyTest1.java
@@ -0,0 +1,16 @@
+public class MyTest1 {
+    public static void main(String[] args) {
+        int a = 1 + (int)(3*Math.random());
+        System.out.println(a);
+        if(a>2){
+            System.out.println("yes");
+        }else if(a<2){
+            System.out.println("no");
+            System.out.println(
+                    "nihaogag" + "fjalkdgnadsng"
+            );
+            // fsadflkgad
+            // fsadflkga
+        }
+    }
+}
